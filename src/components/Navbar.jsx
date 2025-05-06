707039b93809a73f321bcfec3fb92c0fa522c387
Shashank Tripathi <shashanktripathi1852008@gmail.com>
2025-05-06 21:35:07 +0530

    8th commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,11 @@ const Navbar = () => {
     <header>
       <nav>
         <img src={appleImg} alt="Apple" width={14} height={18} />
+        <div>
+          {["Phones", "Macbooks", "Tablets"].map((nav, i) => (
+            <div>{nav}</div>
+          ))}
+        </div>
       </nav>
     </header>
   );
