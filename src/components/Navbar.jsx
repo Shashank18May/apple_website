48e1c8ef9b3ce9741030ab3d3880a989bd8a2ef4
Shashank Tripathi <shashanktripathi1852008@gmail.com>
2025-05-09 18:31:27 +0530

    12th commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,11 +8,16 @@ const Navbar = () => {
       <nav className="flex w-full screen-max-width">
         <img src={appleImg} alt="Apple" width={14} height={18} />
         <div className="flex flex-1 justify-center max-sm:hidden">
-          {["Phones", "Macbooks", "Tablets"].map((nav) => (
-            <div key={nav}>{nav}</div>
+          {navLists.map((nav) => (
+            <div
+              key={nav}
+              className="px-5 text-sm cursor-pointer text-gray hover:text-white transition:all"
+            >
+              {nav}
+            </div>
           ))}
         </div>
-        <div>
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
           <img src={searchImg} alt="search" width={18} height={18} />
           <img src={bagImg} alt="bag" width={18} height={18} />
         </div>
