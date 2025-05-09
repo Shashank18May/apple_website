c34f180ebf4f3751874c32310525aae149b37930
Shashank Tripathi <shashanktripathi1852008@gmail.com>
2025-05-09 18:26:29 +0530

    11th commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { appleImg, bagImg, searchImg } from "../utils";
-import { useEffect } from "react";
+import { navLists } from "../constants";
 
 const Navbar = () => {
   return (
-    <header>
-      <nav>
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+      <nav className="flex w-full screen-max-width">
         <img src={appleImg} alt="Apple" width={14} height={18} />
-        <div>
+        <div className="flex flex-1 justify-center max-sm:hidden">
           {["Phones", "Macbooks", "Tablets"].map((nav) => (
             <div key={nav}>{nav}</div>
           ))}
